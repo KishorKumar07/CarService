9363457ab30ab5d4a994d2944315484a1f7920f5
Kishor Kumar <kishorr21cse@gmail.com>
2023-10-18 13:44:15 +0530

    All done

---
+++ Frontend/src/components/StyleLight1.js
@@ -0,0 +1,18 @@
+import { useMemo } from "react";
+import "./StyleLight1.css";
+const StyleLight1 = ({ styleLightPosition, styleLightTop, styleLightLeft }) => {
+  const styleLight1Style = useMemo(() => {
+    return {
+      position: styleLightPosition,
+      top: styleLightTop,
+      left: styleLightLeft,
+    };
+  }, [styleLightPosition, styleLightTop, styleLightLeft]);
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default StyleLight1;
